8dcd150dc4fae2bd525d57cb379c911f363711ab
devsafix <devsafix@gmail.com>
2025-10-03 00:41:59 +0600

    Create: Action for update blog

--- src/actions/blogActions.ts
+++ src/actions/blogActions.ts
@@ -34,4 +34,31 @@ export async function createBlogAction(values: z.infer<typeof blogSchema>) {
   }
 }
 
+// UPDATE action
+export async function updateBlogAction(
+  id: string,
+  values: z.infer<typeof blogSchema>
+) {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessTokenPortfolio");
+
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/blogs/${id}`, {
+      method: "PATCH",
+      headers: {
+        "Content-Type": "application/json",
+        Cookie: `accessTokenPortfolio=${token?.value}`,
+      },
+      body: JSON.stringify(values),
+    });
+    const data = await res.json();
+    if (!res.ok) throw new Error(data.message || "Failed to update blog.");
+
+    revalidateTag("blogs");
+    return { success: true, message: "Blog updated successfully!" };
+  } catch (error: any) {
+    return { success: false, message: error.message };
+  }
+}
+
 
