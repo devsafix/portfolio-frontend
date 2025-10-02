d76fb50499f13b24ce41b56cd6b64cfb22a3432e
devsafix <devsafix@gmail.com>
2025-10-02 16:04:54 +0600

    Add: Sidebar for dashboard

--- src/components/shared/Sidebar.tsx
+++ src/components/shared/Sidebar.tsx
@@ -1,5 +1,50 @@
 "use client";
 
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+import { Home, PlusCircle, LogOut } from "lucide-react";
+
 export default function Sidebar() {
-  return <div></div>;
+  return (
+    <aside className="flex h-screen w-64 flex-col border-r bg-black text-white">
+      {/* logo */}
+      <div></div>
+      {/* Top navigation */}
+      <nav className="flex-1 space-y-2 p-4">
+        <Link
+          href="/"
+          className="flex items-center gap-2 rounded-lg px-3 py-2 text-sm font-medium hover:bg-gray-100 hover:text-black"
+        >
+          <Home className="h-4 w-4" />
+          Home
+        </Link>
+
+        <Link
+          href="/dashboard/all-projects"
+          className="flex items-center gap-2 rounded-lg px-3 py-2 text-sm font-medium hover:bg-gray-100 hover:text-black"
+        >
+          <PlusCircle className="h-4 w-4" />
+          All Projects
+        </Link>
+        <Link
+          href="/dashboard/all-blogs"
+          className="flex items-center gap-2 rounded-lg px-3 py-2 text-sm font-medium hover:bg-gray-100 hover:text-black"
+        >
+          <PlusCircle className="h-4 w-4" />
+          All Blogs
+        </Link>
+      </nav>
+
+      {/* Bottom action */}
+      <div className="p-4 border-t border-gray-500">
+        <Button
+          variant="destructive"
+          className="w-full justify-start gap-2 cursor-pointer"
+        >
+          <LogOut className="h-4 w-4" />
+          Logout
+        </Button>
+      </div>
+    </aside>
+  );
 }
