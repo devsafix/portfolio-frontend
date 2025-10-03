b8bb583832b8b6832158c4e45e00cbac813ecc3b
devsafix <devsafix@gmail.com>
2025-10-03 11:35:21 +0600

    Update: about info page in dashboard

--- src/app/(dashboard)/dashboard/about-info/page.tsx
+++ src/app/(dashboard)/dashboard/about-info/page.tsx
@@ -1,7 +1,31 @@
-import React from 'react'
+import { TAbout } from "@/types";
+import AboutForm from "@/components/module/Dashboard/AboutForm";
+import { cookies } from "next/headers";
 
-export default function AboutInfoPage() {
+// Fetcher function to get the current about data
+async function getAboutData(): Promise<TAbout | null> {
+  const cookieStore = await cookies();
+  const token = cookieStore.get("accessTokenPortfolio");
+  try {
+    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_API}/about`, {
+      headers: { Cookie: `accessTokenPortfolio=${token?.value}` },
+      cache: "no-store",
+    });
+    if (!res.ok) return null;
+    const data = await res.json();
+    return data.data;
+  } catch (error) {
+    console.error("Failed to fetch about data:", error);
+    return null;
+  }
+}
+
+export default async function AboutInfoPage() {
+  const aboutData = await getAboutData();
+
   return (
-    <div>AboutInfoPage</div>
-  )
+    <div>
+      <AboutForm aboutData={aboutData} />
+    </div>
+  );
 }
